04208603e1847582d0ccb7fab07950a77c63265f
mnyon-grandkru <me@mnyon.com>
2023-06-10 19:21:11 +0000

    Put fetch logic into client

--- client.js
+++ client.js
@@ -1,3 +1,24 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch'
+
+function fetchThis(url) {
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        // Status code is within the 2xx range
+        return response.json();
+      } else {
+        throw new Error('Request failed with status code ' + response.status);
+      }
+    })
+    .then(data => {
+      // Handle the successful response data
+      console.log('Success:', data);
+    })
+    .catch(error => {
+      // Handle any error that occurred during the request
+      console.error('Error:', error.message);
+    });
+}
 
 
+export default fetchThis;
--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-const fetch = require('node-fetch');
+import fetchThis from './client.js';
 
-const url = 'https://api.apis.guru/v2/list.json'; 
+const url = 'https://api.apis.guru/v2/list.json';
+fetchThis(url);
 
-fetch(url)
-  .then(response => {
-    if (response.ok) {
-      // Status code is within the 2xx range
-      return response.json();
-    } else {
-      throw new Error('Request failed with status code ' + response.status);
-    }
-  })
-  .then(data => {
-    // Handle the successful response data
-    console.log('Success:', data);
-  })
-  .catch(error => {
-    // Handle any error that occurred during the request
-    console.error('Error:', error.message);
-  });
